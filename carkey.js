337271b9fb0f5bd5135460bb4c825e95e8a0a856
iswoad <iswoadansary@gmail.com>
2020-05-02 23:42:29 +0600

    edited carkey

--- carkey.js
+++ carkey.js
@@ -9,28 +9,28 @@
 // 	console.log("bye boss");
 // }
 
-// function checkdriverAge () {
-// 	var age = prompt("what is your age?")
-// 	if (Number(age)<18){
-// 		alert("go ride a cycle");
-// 	} else if (Number(age)>18){
-// 		alert("drive safe sir");
-// 	} else if (Number(age)===18){
-// 		alert("ok you can start the car bruh");
-// 	}
-// }
+function checkdriverAge () {
+	var age = prompt("what is your age?")
+	if (Number(age)<18){
+		alert("go ride a cycle");
+	} else if (Number(age)>18){
+		alert("drive safe sir");
+	} else if (Number(age)===18){
+		alert("ok you can start the car bruh");
+	}
+}
 
-// var checkdriverAge2 = function () {
-// 	var age = prompt("Tell me your age nigger")
-// 	if (Number(age)<18){
-// 		alert("go ride a cycle");
-// 	} else if (Number(age)>18){
-// 		alert("drive safe sir");
-// 	} else if (Number(age)===18){
-// 		alert("ok you can start the car bruh");
-// 	}
-// }
+var checkdriverAge2 = function () {
+	var age = prompt("Tell me your age nigger")
+	if (Number(age)<18){
+		alert("go ride a cycle");
+	} else if (Number(age)>18){
+		alert("drive safe sir");
+	} else if (Number(age)===18){
+		alert("ok you can start the car bruh");
+	}
+}
 
 // var list = ["banana", "apple", "orange", "blueberries"];
 
-var fruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// var fruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
